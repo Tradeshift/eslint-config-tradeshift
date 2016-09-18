4d2bf51a090459ae23a10eb8098bc1af925261da
Søren Louv-Jansen <sorenlouv@gmail.com>
2016-09-18 23:25:24 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+        "semi": [2, "always"],
+        "no-tabs": 0,
+        "indent": [
+            2,
+            "tab"
+        ]
+    }
+};
