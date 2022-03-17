55a2585a106978ffb4bc00b3cb1329923696648b
Jacob Wejendorp <jwe@tradeshift.com>
2022-03-17 09:37:09 +0100

    Fix JSX syntax after parser changes (#53)

--- test/validate-config.js
+++ test/validate-config.js
@@ -1,16 +1,16 @@
 const { ESLint } = require('eslint');
 const test = require('tape');
 
-test('load config in eslint to validate all rule syntax is correct', async function(t) {
+test('load config in eslint to validate all rule syntax is correct', async function (t) {
 	const eslint = new ESLint({
 		useEslintrc: false,
 		baseConfig: {
 			extends: [
 				require.resolve('../'),
 				require.resolve('../jest'),
-				require.resolve('../typescript')
-			]
-		}
+				require.resolve('../typescript'),
+			],
+		},
 	});
 
 	const code = `
@@ -29,5 +29,32 @@ test('load config in eslint to validate all rule syntax is correct', async funct
 
 	t.deepEqual(resultsJS[0].messages, []);
 	t.deepEqual(resultsTS[0].messages, []);
+	t.end();
+});
+
+test('check for JSX support', async function (t) {
+	const eslint = new ESLint({
+		useEslintrc: false,
+		baseConfig: {
+			extends: [require.resolve('../'), require.resolve('../typescript')],
+		},
+	});
+
+	const code = `
+export const AwesomeJSX = () => (
+	<section className="jsx-section">
+		woop
+	</section>
+);
+`;
+
+	const resultsJS = await eslint.lintText(code, { filePath: 'index.js' });
+	t.deepEqual(resultsJS[0].messages, [], 'Should parse JSX in *.js');
+	const resultsJSX = await eslint.lintText(code, { filePath: 'index.jsx' });
+	t.deepEqual(resultsJSX[0].messages, [], 'Should parse JSX in *.jsx');
+	// Note: typescript conventions dont allow JSX in *.ts files
+	const resultsTSX = await eslint.lintText(code, { filePath: 'index.tsx' });
+	t.deepEqual(resultsTSX[0].messages, [], 'Should parse JSX in *.tsx');
+
 	t.end();
 });
